43383c8ca17f1d30ccdc5e879953cb00abf2fbcb
KIEUDAIK <kieungothekieu@gmail.com>
2022-03-24 17:00:18 +0700

    task/init_project: register service at application

---
+++ Employee.Application/Services/ApplicationServiceExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Employee.Application.Validations;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using FluentValidation;
+using Employee.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Employee.Application.Services
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static void AddApplicationService(IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+        }
+    }
+}
